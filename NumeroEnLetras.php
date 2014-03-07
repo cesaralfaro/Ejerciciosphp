0b47257ee12cb926673540f15f25f37d4b614cca
cesaralfaro <cesaralsa1988@hotmail.com>
2014-03-07 13:48:57 +0100

    Actualizo
    Ya lee numeros 999 a 0

--- NumeroEnLetras.php
+++ NumeroEnLetras.php
@@ -1,21 +1,27 @@
 <?PHP
 
-$numero=526;
-$UnoADiez=array("uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve");
+$numero=999;
+$UnoADiez=array("cero","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve");
 $Decenas=array("dieci","veinti","treinta y ","cuarenta y ","cincuenta y ","sesenta y ","setenta y ","ochenta y ","noventa y ");
-$OnceAQuince=array("once","doce","trece","catorce","quince");
+$DiezAQuince=array("diez","once","doce","trece","catorce","quince");
 $Centenas=array("ciento","doscientos","trescientos","cuatrocientos","quinientos","seiscientos","setecientos","ochocientos","novecientos");
 
 if ($numero>=100){
-	echo $Centenas[round($numero/100)-1]." "; 
+	echo $Centenas[floor($numero/100)-1]." "; 
 	$numero=$numero%100;
-	}
+}
 if ($numero>=10){
-echo number_format(($numero/10),0);
-	echo $Decenas[number_format(($numero/10),0)-1];
+	if ($numero<=15){
+		echo $DiezAQuince[$numero-10];
+	}
+	else{
+	echo $Decenas[floor($numero/10)-1];
 	$numero=$numero%10;
 	}
-
+}
+if ($numero<10) {
+	echo $UnoADiez[$numero];
+}
 	
 
 ?>
