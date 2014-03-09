2e22bb32e16fda3de84a6bd9b225b049092264d2
cesaralfaro <cesaralsa1988@hotmail.com>
2014-03-09 19:46:00 +0100

    Terminado
    Ya funciona para todos los números de 0 a 999.999.999

--- NumeroEnLetras.php
+++ NumeroEnLetras.php
@@ -1,27 +1,67 @@
 <?PHP
 
-$numero=999;
-$UnoADiez=array("cero","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve");
-$Decenas=array("dieci","veinti","treinta y ","cuarenta y ","cincuenta y ","sesenta y ","setenta y ","ochenta y ","noventa y ");
+$numero=6784920;
+
+function LeeNumero($cantidad) {
+
+$UnoADiez=array("uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve");
 $DiezAQuince=array("diez","once","doce","trece","catorce","quince");
+$Decenas=array("dieci","veinti","treinta y ","cuarenta y ","cincuenta y ","sesenta y ","setenta y ","ochenta y ","noventa y ");
+$DecenasExactas=array("veinte","treinta","cuarenta","cincuenta","sesenta","setenta","ochenta","noventa");
 $Centenas=array("ciento","doscientos","trescientos","cuatrocientos","quinientos","seiscientos","setecientos","ochocientos","novecientos");
 
-if ($numero>=100){
-	echo $Centenas[floor($numero/100)-1]." "; 
-	$numero=$numero%100;
+	if ($cantidad>=100){
+		if($cantidad==100){  //Si es 100 exacto se lee cien y no ciento
+			echo "cien";
+		}
+		else{
+			echo $Centenas[floor($cantidad/100)-1]." "; 
+			$cantidad=$cantidad%100;
+		}
+	}
+	if ($cantidad>=10 && $cantidad<100){
+		if ($cantidad<=15){						//Entre 10 y 15 se leen diferente
+			echo $DiezAQuince[$cantidad-10];
+		}
+		elseif ($cantidad%10==0){				//Para numeros con unidades igual a cero se lee diferente
+			echo $DecenasExactas[$cantidad/10-2];
+		}
+		else {
+			echo $Decenas[floor($cantidad/10)-1];
+			$cantidad=$cantidad%10;
+		}
+	}
+	if ($cantidad>0 && $cantidad<10) {
+		echo $UnoADiez[$cantidad-1];	
+	}
 }
-if ($numero>=10){
-	if ($numero<=15){
-		echo $DiezAQuince[$numero-10];
+
+if($numero==0){
+	echo "cero";
+}
+
+if($numero>999999){
+	$millones=floor($numero/1000000);
+	$numero=$numero-$millones*1000000;
+	if($millones==1){
+		echo "un millon ";
 	}
 	else{
-	echo $Decenas[floor($numero/10)-1];
-	$numero=$numero%10;
+		LeeNumero($millones);
+		echo " millones ";
 	}
 }
-if ($numero<10) {
-	echo $UnoADiez[$numero];
+
+if ($numero>999){
+	$miles=floor($numero/1000);
+	if($miles>=2){
+		LeeNumero($miles);
+	}
+	echo " mil ";
+	$numero=$numero-$miles*1000;
 }
-	
+
+LeeNumero($numero);
+
 
 ?>
