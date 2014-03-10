935f7d3684334ae70a8bc73b0fab3c316eb4f15c
cesaralfaro <cesaralsa1988@hotmail.com>
2014-03-10 13:48:40 +0100

    Comunes
    Calcula minimo común múltiplo y máximo común denominador

---
+++ Comunes.php
@@ -0,0 +1,56 @@
+<?PHP
+
+$num1=3;
+$num2=10;
+$num3=13;
+
+function descomponer($cantidad){
+	$descomposicion[0:$cantidad]=0;
+}
+
+/*for($i=2;$i<=$num1;$i++){
+	$divisoresNum1[$i-2]=0;
+}
+
+for($i=2;$i<=$num1;$i++){
+	echo $divisoresNum1[$i-2];
+}
+
+for($i=2;$i<=$num2;$i++){
+	$divisoresNum2[$i-2]=0;
+}
+
+for($i=2;$i<=$num3;$i++){
+	$divisoresNum3[$i-2]=0;
+}
+
+for($i=2;$i<=$num1;$i++){
+	if($num1%$i==0){
+		$num1=$num1/$i;
+		$divisoresNum1[$i-2]++;
+	}
+}
+
+
+
+for($i=2;$i<=$num2;$i++){
+	if($num2%$i==0){
+		$num2=$num2/$i;
+		$divisoresNum2[$i-2]++;
+	}
+}
+for($i=2;$i<=$num2;$i++){
+	echo $divisoresNum2[$i-2]." ";
+}
+
+for($i=2;$i<=$num3;$i++){
+	if($num3%$i==0){
+		$num3=$num3/$i;
+		$divisoresNum3[$i-2]++;
+	}
+}
+for($i=2;$i<=$num3;$i++){
+	echo $divisoresNum3[$i-2]." ";
+}*/
+
+?>
